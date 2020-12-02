4fc9fc6d7b13c578a7497b5501914b1140fde24f
PikarryPham <phamngocthuytrang1026@gmail.com>
2020-12-03 00:07:45 +0700

    upload template/upload models/fix db

---
+++ models/Course.js
@@ -0,0 +1,88 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: () => {
+      return this.name != "";
+    },
+  },
+  content: {
+    type: String,
+    required: () => {
+      return this.content != "";
+    },
+  },
+  description: {
+    type: String,
+    required: () => {
+      return this.description != "";
+    },
+  },
+  numOfStudentRating: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  numOfStudentRegister: {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  basicPrice: {
+    type: Number,
+    required: true,
+    min: 9,
+  },
+  lastUpdate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  postDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  status: {
+    type: Number,
+    required: () => {
+      return this.status != "";
+    },
+  },
+  img: {
+    type: String,
+    required: false,
+  },
+  salePrice: {
+    type: Number,
+    required: true,
+    min: 9,
+  },
+  beRegisted: [
+    {
+      /**
+       * Date registed??? nen luu ntn
+       */
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  beLoved: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  beTeached: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  /**
+   * Rated???
+   */
+});
+
+const User = mongoose.model("Course", courseSchema);
+
+module.exports = Course;
---
+++ models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    required: () => {
+      return this.this.password != "";
+    },
+    minlength: 6,
+  },
+  name: {
+    type: String,
+    required: false,
+  },
+  typeOfUser: {
+    type: Number,
+    required: true,
+  },
+  registed: [
+    {
+      /**
+       * date registed?
+       */
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+  love: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+  teach: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
