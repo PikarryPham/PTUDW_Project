27d614c9c59f986fa9523f88dd296d2f48933dd0
PikarryPham <phamngocthuytrang1026@gmail.com>
2021-01-13 12:10:53 +0700

    note about missing features

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,64 +1,61 @@
 const catchAsync = require("../utils/catchAsync");
-const {
-    User,
-    Course
-} = require("../models");
+const { User, Course } = require("../models");
 const Orders = require("../models/Orders");
 
 exports.getIndexAdminProfile = catchAsync(async (req, res, next) => {
-    const courses = await Course.find().lean()
+  const courses = await Course.find().lean();
 
-    const user = await User.findById(req.signedCookies.jwt).lean()
-    res.render('admin/courses', {
-        layout: false,
-        user,
-        courses,
-        error: req.session.error
-    })
-    req.session.destroy();
-})
+  const user = await User.findById(req.signedCookies.jwt).lean();
+  res.render("admin/courses", {
+    layout: false, //note lại phần này tại sao layout = true thì k chạy?
+    user,
+    courses,
+    error: req.session.error,
+  });
+  req.session.destroy();
+});
 exports.getIndexUser = catchAsync(async (req, res, next) => {
-    const users = await User.find({role: 'user'}).lean()
-    const user = await User.findById(req.signedCookies.jwt).lean()
-    res.render('admin/users', {
-        layout: false,
-        user,
-        users
-    })
-})
+  const users = await User.find({ role: "user" }).lean();
+  const user = await User.findById(req.signedCookies.jwt).lean();
+  res.render("admin/users", {
+    layout: false,
+    user,
+    users,
+  });
+});
 
-exports.getIndexInstructor = catchAsync(async(req,res,next) => {
-     const users = await User.find({role: 'instructors'}).lean()
-    const user = await User.findById(req.signedCookies.jwt).lean()
-    res.render('admin/users', {
-        layout: false,
-        user,
-        users
-    })
-})
-exports.deleteIndexUser = catchAsync(async(req,res,next) => {
-    await User.findByIdAndDelete(req.params.idUser);
-    res.redirect("/admin")
-})
-exports.deleteIndexCourse = catchAsync(async(req,res,next) => {
-    const course = Orders.findOne({
-        course: req.params.idCourse
-    });
-    if(course.enrolled) {
-         req.session.error = 'Course have User learning not delete'
-        res.redirect('/admin')
-        return;
-    }
-    await Course.findByIdAndDelete(req.params.idCourse);
-    res.redirect("/admin")
-})
+exports.getIndexInstructor = catchAsync(async (req, res, next) => {
+  const users = await User.find({ role: "instructors" }).lean();
+  const user = await User.findById(req.signedCookies.jwt).lean();
+  res.render("admin/users", {
+    layout: false,
+    user,
+    users,
+  });
+});
+exports.deleteIndexUser = catchAsync(async (req, res, next) => {
+  await User.findByIdAndDelete(req.params.idUser);
+  res.redirect("/admin");
+});
+exports.deleteIndexCourse = catchAsync(async (req, res, next) => {
+  const course = Orders.findOne({
+    course: req.params.idCourse,
+  });
+  if (course.enrolled) {
+    req.session.error = "Course have User learning not delete";
+    res.redirect("/admin");
+    return;
+  }
+  await Course.findByIdAndDelete(req.params.idCourse);
+  res.redirect("/admin");
+});
 
-exports.getIndexCategory = catchAsync(async (req,res,next) => {
-    const user = await User.findById(req.signedCookies.jwt).lean();
-    
-    res.render('admin/add-category', {
-        layout: false,
-        user,
-        error: req.session.error
-    })
-})
+exports.getIndexCategory = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.signedCookies.jwt).lean();
+
+  res.render("admin/add-category", {
+    layout: false,
+    user,
+    error: req.session.error,
+  });
+});
