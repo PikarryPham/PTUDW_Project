3b0825699cfd6b939252d40d2c2e2e77f2c07ec3
PikarryPham <phamngocthuytrang1026@gmail.com>
2021-01-14 22:14:12 +0700

    debug lan 5

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,50 +1,60 @@
 const catchAsync = require("../utils/catchAsync");
 const { User, Lesson, Course, Video } = require("../models/index");
 
-exports.getIndexAddVideo = catchAsync(async (req, res, next) => {
-  const { idCourse } = req.params;
-  const user = await User.findById(req.user.id).lean();
-  const lessons = await Lesson.find({
-    idCourse: idCourse,
-  })
-    .populate({
-      path: "idCourse",
-      match: {
-        instructors: req.user.id,
-      },
-      select: "instructors",
+exports.getIndexAddVideo = async (req, res, next) => {
+  try {
+    const { idCourse } = req.params;
+    const user = await User.findById(req.user.id).lean();
+    const lessons = await Lesson.find({
+      idCourse: idCourse,
     })
-    .lean();
-  const course = await Course.findById({
-    _id: idCourse,
-    instructors: req.user.id,
-  });
-  if (!lessons || !course) {
+      .populate({
+        path: "idCourse",
+        match: {
+          instructors: req.user.id,
+        },
+        select: "instructors",
+      })
+      .lean();
+    const course = await Course.findById({
+      _id: idCourse,
+      instructors: req.user.id,
+    });
+    if (!lessons || !course) {
+      res.redirect("/profile");
+      return;
+    }
+    res.render("instructors/add-video", {
+      user,
+      layout: false,
+      lessons,
+      idCourse,
+    });
+  } catch (err) {
+    req.session.error = err.message;
     res.redirect("/profile");
-    return;
   }
-  res.render("instructors/add-video", {
-    user,
-    layout: false,
-    lessons,
-    idCourse,
-  });
-});
+};
 
-exports.postAddVideo = catchAsync(async (req, res, next) => {
-  const { idCourse } = req.params;
+exports.postAddVideo = async (req, res, next) => {
+  try {
+    const { idCourse } = req.params;
 
-  const lessons = await Lesson.findById(req.body.idLesson);
-  const course = await Course.findOne({
-    _id: idCourse,
-    instructors: req.user.id,
-  });
-  if (!lessons || !course) {
+    const lessons = await Lesson.findById(req.body.idLesson);
+    const course = await Course.findOne({
+      _id: idCourse,
+      instructors: req.user.id,
+    });
+    if (!lessons || !course) {
+      res.redirect("/profile");
+      return;
+    }
+    req.body.isLooked = req.body.isLooked === "true" ? true : false;
+    req.body.pathUrl = req.file.path.split("/").slice(1).join("/");
+    await Video.create(req.body);
     res.redirect("/profile");
-    return;
+  } catch (err) {
+    req.session.error = err.message;
+    res.redirect("/profile");
   }
-  req.body.isLooked = req.body.isLooked === "true" ? true : false;
-  req.body.pathUrl = req.file.path.split("/").slice(1).join("/");
-  await Video.create(req.body);
-  res.redirect("/profile");
-});
+};
